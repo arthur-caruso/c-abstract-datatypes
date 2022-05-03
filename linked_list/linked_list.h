d7cd0d2493cd62989c25d97120e51a12c62ad951
arthur-caruso <101721229+arthur-caruso@users.noreply.github.com>
2022-05-03 09:08:58 -0300

    Add delete_node function

--- linked_list/linked_list.h
+++ linked_list/linked_list.h
@@ -8,6 +8,7 @@ typedef struct node {
 
 void print_list(node_t *head);
 node_t *create_node(int value);
+int delete_node(node_t *head, node_t *del_node);
 node_t *insert_at_head(node_t **head, node_t *node_to_insert);
 node_t *insert_at_tail(node_t *head, node_t *node_to_insert);
 node_t *find_node(node_t *head, int value);
