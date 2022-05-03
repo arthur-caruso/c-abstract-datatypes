2d432a4fc7c91b7bda74b86259cddb37eae16579
arthur-caruso <101721229+arthur-caruso@users.noreply.github.com>
2022-05-03 09:30:20 -0300

    Update main.c

--- linked_list/main.c
+++ linked_list/main.c
@@ -4,18 +4,48 @@ int main() {
 	node_t *head = NULL;
 	node_t *tmp;
 
+	// adding some values into the list
 	for (int i = 0; i < 10; i++) {
 		tmp = create_node(i);
 		insert_at_head(&head, tmp);
 		tmp = create_node(i);
 		insert_at_tail(head, tmp);
 	}
-
-	tmp = create_node(69);
+	
+	// creating a node with value 68 at tail of list
+	int value = 68;
+	
+	tmp = create_node(value);
 	insert_at_tail(head, tmp);
-	tmp = find_node(head, 69);
+	
+	// printing list
+	print_list(head);
+	printf("\n");
 
-	printf("found the node %p with value %d\n", tmp, tmp->value);
+	// searching vor value 68 on list
+	tmp = find_node(head, value);
+	if (tmp != NULL)
+		printf("found the node %p with value %d\n", tmp, tmp->value);
+	else
+		printf("could not locate node of value %d\n", value);
+
+	// demonstrating deletion of node
+	if (!delete_node(head, tmp + 1)) // does not point to a node
+		printf("could not delete node of address %p\n", tmp + 1);
+	else printf("successfully deleted node of address %p\n", tmp + 1);
+
+	if (!delete_node(head, tmp)) // points to node of value 68
+		printf("could not delete node of address %p\n", tmp);
+	else printf("successfully deleted node of address %p\n", tmp);
+
+	// searching vor value 68 on list, once again
+	tmp = find_node(head, value);
+	if (tmp != NULL)
+		printf("found the node %p with value %d\n", tmp, tmp->value);
+	else
+		printf("Could not locate node of value %d\n", value);
+
+	// printing list
 	print_list(head);
 	printf("\n");
 
