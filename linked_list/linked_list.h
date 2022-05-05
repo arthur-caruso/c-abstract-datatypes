fc4ff362299b2d0a5d984a6ffa88c848782c2bb6
arthur-caruso <101721229+arthur-caruso@users.noreply.github.com>
2022-05-05 08:39:18 -0300

    Update linked_list.h

--- linked_list/linked_list.h
+++ linked_list/linked_list.h
@@ -8,7 +8,7 @@ typedef struct node {
 
 void print_list(node_t *head);
 node_t *create_node(int value);
-int delete_node(node_t *head, node_t *del_node);
-int insert_at_head(node_t **head, node_t *node_to_insert);
-int insert_at_tail(node_t *head, node_t *node_to_insert);
+int delete_node(node_t *head, node_t *node_del);
+int insert_at_head(node_t **head, node_t *node_nsrt);
+int insert_at_tail(node_t *head, node_t *node_nsrt);
 node_t *find_node(node_t *head, int value);
